85a7f05364c18facc5e90e99dde62357d5608435
aselwilliams <aselwilliams22@gmail.com>
2022-09-26 20:38:23 -0500

    fixed navigation bug

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -9,7 +9,7 @@ function ScrollToTop() {
   });
   return (
     <Div>
-      <a href="#" className={`${visible ? "block" : " none"}`}>
+      <a href="#home" className={`${visible ? "block" : " none"}`}>
         <FaChevronUp />
       </a>
     </Div>
